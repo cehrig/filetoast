5fad36e0ab46ceec4f338bd0a732216f521c5f98
Christian Ehrig <christian.ehrig@eog-group.com>
2016-02-10 17:56:54 +0100

    removed timeout curlopt

--- src/worker.c
+++ src/worker.c
@@ -106,8 +106,8 @@ int senddata(t_job * job)
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post);
-        curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L);
-        curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
+        //curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L);
+        //curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
         res = curl_easy_perform(curl);
 
         if(res != CURLE_OK) {
